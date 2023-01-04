8e3a83adfbebb1806735cae492688f8238e59054
Brandon Croft <brandon.croft@gmail.com>
2023-01-03 18:29:10 -0700

    load bundle from correct directory, ruby 3.2+ compatibility

--- lib/go_version/wrapper.rb
+++ lib/go_version/wrapper.rb
@@ -4,7 +4,7 @@ module GoVersion
   module Wrapper
     extend FFI::Library
 
-    ffi_lib File.join(File.expand_path(__dir__), "../../ext/go-version/go_version.bundle")
+    ffi_lib File.join(File.expand_path(__dir__), "go_version.bundle")
     attach_function :Check, [:string, :string], :bool
   end
 end
