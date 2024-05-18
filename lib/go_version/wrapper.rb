8c973865a5c52ad88fbcf5bcbb9792fdc540c421
Brandon Croft <brandon.croft@gmail.com>
2024-05-18 05:59:17 -0600

    Update wrapper.rb

--- lib/go_version/wrapper.rb
+++ lib/go_version/wrapper.rb
@@ -5,9 +5,9 @@ module GoVersion
     extend FFI::Library
 
     begin
-      ffi_lib File.join(File.expand_path(__dir__), "../../lib/go_version/go_version.bundle")
+      ffi_lib File.join(File.expand_path(__dir__), "go_version.bundle")
     rescue LoadError
-      ffi_lib File.join(File.expand_path(__dir__), "../../ext/go_version/go_version.bundle")
+      ffi_lib File.join(File.expand_path(__dir__), "go_version.so")
     end
     attach_function :Check, [:string, :string], :bool
   end
